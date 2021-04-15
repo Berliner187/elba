34bb4588fba05fbb23dd8ee30d57ff8cfe03fa4e
Berliner187 <fadenougat@gmail.com>
2021-04-15 23:18:55 +0300

    Fix update module

--- update_obs.py
+++ update_obs.py
@@ -1,23 +1,19 @@
-def system_action(action):
-    """ Restart Program or Clear terminal """
-    if action == 'restart':
-        os.execv(sys.executable, [sys.executable] + sys.argv)
-    elif action == 'clear':
-        os.system('cls' if os.name == 'nt' else 'clear')
-    elif action == 'either':
-        os.system('cls' if os.name == 'nt' else 'clear')
-        os.execv(sys.executable, [sys.executable] + sys.argv)
+from main import system_action, show_decryption_data, decryption_block
+from enc_obs import dec_data
+import os
 
+
+yellow, mc = "\033[33m", "\033[0m"
 
 def update(master_password, status):
     main_file = 'main.py'  # Главный файл программы
-    remove_main_folder = 'rm -r pwManager/ -f'
-    os.system('git clone https://github.com/Berliner187/pwManager')
+    remove_main_folder = 'rm -r elba/ -f'
+    os.system('git clone https://github.com/Berliner187/elba')
     system_action('clear')
 
-    if os.path.getsize(main_file) != os.path.getsize('pwManager/' + main_file):
+    if os.path.getsize(main_file) != os.path.getsize('elba/' + main_file):
         install_or_no = input(yellow + ' - Install? (y/n): ' + mc)
-        new_folder_pm = 'pwManager/'    # Новая папка из репозитория проекта
+        new_folder_pm = 'elba/'    # Новая папка из репозитория проекта
         if install_or_no == 'y':
 
             def actions_for_install(file):  # Действия для установки
@@ -27,13 +23,12 @@ def update(master_password, status):
             actions_for_install('stars_obs.py')
             actions_for_install('enc_obs.py')
             actions_for_install('datetime.py')
+            actions_for_install('logo_obs.py')
+            actions_for_install('update_obs.py')
 
             system_action('either')
         else:
             os.system(remove_main_folder)
-            if status == bool(True):
-                show_decryption_data(master_password)
-                decryption_block(master_password)
     else:
         system_action('clear')
         print(yellow + ' -- Nothing to upgrade, you have latest update -- ' + mc)
