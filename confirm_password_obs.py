626612ddf53c8c1bcb42bbb4ba26f6d9abb15dfb
Berliner187 <fadenougat@gmail.com>
2021-04-17 21:09:48 +0300

    Add modules with password

---
+++ confirm_password_obs.py
@@ -0,0 +1,62 @@
+from stars_obs import hide_password
+import random
+from time import sleep
+
+# List of all symbols for password
+symbols_for_password = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_-='
+
+yellow, blue, purple, green, mc, red = "\033[33m", "\033[36m", "\033[35m", "\033[32m", "\033[0m", "\033[31m"
+
+
+def confirm_user_password(type_pas):
+    """ Подтвержение пользовательского пароля """
+    def user_input_password():
+        print(blue + '\n Minimum password length 8 characters' + mc)
+        user_password = hide_password('\n Password: ')
+        if user_password == 'x':
+            quit()
+        user_confirm_password = hide_password(' Confirm password: ')  # hide_password(' Confirm password: ')
+        if user_password != user_confirm_password or len(user_password) < 8:
+            print(red + '\n Error of confirm. Try again \n' + mc)
+            user_input_password()
+        else:
+            return user_password
+
+    def generation_new_password():
+        """ Генерирование нового случайного пароля """
+        length_new_pas = int(input(yellow + ' - Length password (Minimum 8): ' + mc))
+        new_password = ''  # Empty password
+        for pas_elem in range(length_new_pas):
+            new_password += random.choice(symbols_for_password)  # Password Adding random symbols from lister
+        if len(new_password) > 8:
+            return new_password  # Возвращает пароль
+        else:
+            print(red + '\n Error of confirm. Try again \n' + mc)
+            generation_new_password()
+
+    # Условаия принятия и подтверждения пароля
+    if type_pas == 'self':  # Собсвенный пароль для ресурса
+        password = user_input_password()
+        print(blue + ' - Your password success saved' + mc)
+        sleep(1)
+        return password
+    elif type_pas == 'master':  # Мастер пароль
+        master_password = user_input_password()
+        # Проверка хеша пароля
+        if check_file_hash_password == bool(False) and check_file_date_base == bool(False):  # Создание хэша
+            hash_to_file = generate_password_hash(master_password)
+            with open(file_hash_password, 'w') as hash_pas:
+                hash_pas.write(hash_to_file)
+                hash_pas.close()
+            return master_password
+        elif check_file_hash_password == bool(False) and check_file_date_base == bool(True):
+            print(red + ' - Not confirmed - ' + mc)
+            sleep(2)
+            quit()
+        elif check_file_date_base and check_file_hash_password == bool(True):
+            return master_password
+    elif type_pas == 'gen_new':     # Генерирование нового пароля
+        password = generation_new_password()
+        print(blue + ' - Your new password - ' + green + password + mc + ' - success saved' + mc)
+        sleep(2)
+        return password
