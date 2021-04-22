ac08e044e7bbbcf74b06ff9a8a9f17da9cfce3da
Rostislav <fadenougat@gmail.com>
2021-04-22 18:14:14 +0300

    Добавлены исключения в обновлении

--- update_obs.py
+++ update_obs.py
@@ -17,14 +17,8 @@ stock_modules = ['datetime_obs.py', 'enc_obs.py', 'logo_obs.py',
 def update():   # Обновление программы
     main_file = 'main.py'  # Главный файл программы
     new_folder_el = 'elba/'  # Новая папка из репозитория проекта
-    remove_main_folder = 'rm -r elba/ -f'  # Удаление новой папки
-    if os.path.exists(new_folder_el):
-        download_from_repository()
-
-    def actions_for_install(program_file):  # Действия для установки
-        os.system('cp ' + new_folder_el + program_file + ' . ; ')
-
-    system_action('clear')
+    remove_main_folder = 'rm -r ' + new_folder_el + ' -f'  # Удаление новой папки
+    download_from_repository()  # Загрузка проекта из репозитория
 
     cnt_modules = 0     # Счетчик отсутствующих модулей
     file_type = 'obs.py'    # Модули заканчиваются на *obs.py
@@ -36,7 +30,10 @@ def update():   # Обновление программы
     for j in range(len(stock_modules)):
         if stock_modules[j] not in modules:
             cnt_modules += 1
-    print(cnt_modules)
+
+    def actions_for_install(program_file):  # Действия для установки
+        os.system('cp ' + new_folder_el + program_file + ' . ; ')
+
     if cnt_modules != 0:
         system_action('clear')
 
@@ -55,6 +52,7 @@ def update():   # Обновление программы
             else:   # Вывод состояния ОК
                 template_text_modules(green, 'OK')
                 sleep(.5)
+
         for i in range(len(stock_modules)):
             actions_for_install(stock_modules[i])
 
@@ -62,23 +60,22 @@ def update():   # Обновление программы
         print(green + '\n The missing module has been installed! \n\n' + mc)
         sleep(1)
         system_action('restart')
+        
+    if os.path.exists(new_folder_el):
+        if os.path.getsize(main_file) != os.path.getsize(new_folder_el + main_file):    # Если размеры файлов не совпадают
+            print(green + '\n   A new version of the program is available ' + mc)
+            install_or_no = input(yellow + ' - Install new version program? (y/n): ' + mc)
+            if install_or_no == 'y':
 
-    if os.path.getsize(main_file) != os.path.getsize(new_folder_el + main_file):    # Если размеры файлов не совпадают
-
-        print(green + '\n   A new version of the program is available ' + mc)
-        install_or_no = input(yellow + ' - Install new version program? (y/n): ' + mc)
-        if install_or_no == 'y':
-
-            actions_for_install(main_file)
-            actions_for_install('update_obs.py')
-            for i in range(len(stock_modules)):
-                actions_for_install(stock_modules[i])
-
-            system_action('restart')
+                actions_for_install(main_file)
+                actions_for_install('update_obs.py')
+                for i in range(len(stock_modules)):
+                    actions_for_install(stock_modules[i])
+                system_action('restart')
+            else:
+                os.system(remove_main_folder)
         else:
+            system_action('clear')
+            print(yellow + ' -- You are using the latest version of the program -- ' + mc)
             os.system(remove_main_folder)
-    else:
-        system_action('clear')
-        print(yellow + ' -- You are using the latest version of the program -- ' + mc)
-        os.system(remove_main_folder)
-        sleep(.7)
+            sleep(.7)
