998411d2c172567563958bc146f0a40b92288a3f
Rostislav <fadenougat@gmail.com>
2021-07-05 21:52:30 +0300

    Изменён дизайн-код

--- show_dec_data_obs.py
+++ show_dec_data_obs.py
@@ -1,20 +1,19 @@
 from enc_obs import *
 
 
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 
-def show_decryption_data(master_password, category):
+def show_decryption_data(generic_key, category):
     system_action('clear')
     separator = ''
     if category == 'note':
-        # Костыль
         separator += '    '
-    print(BLUE, "     ___________________________________")
-    print(BLUE, "    /\/| ", YELLOW, "\/                   \/", BLUE, " |\/\ ")
-    print(BLUE, "   /\/\|", YELLOW, " \/  Saved " + category + 's' + separator + "  \/ ", BLUE, "|/\/\ ", DEFAULT_COLOR)
-    print(YELLOW, "           \/                   \/ ", DEFAULT_COLOR)
-    print('\n'*3)
+    print(BLUE + "     ___________________________________")
+    print(BLUE + "    /\/| ", YELLOW, "\/                   \/", BLUE, " |\/\ ")
+    print(BLUE + "   /\/\|", YELLOW, " \/  Saved " + category + 's' + separator + "  \/ ", BLUE, "|/\/\ ", DEFAULT_COLOR)
+    print(YELLOW + "           \/                   \/ \n"*2)
+    print('\n')
 
     s = 0
 
@@ -25,28 +24,33 @@ def show_decryption_data(master_password, category):
         type_folder = FOLDER_WITH_NOTES
 
     for category_item in os.listdir(type_folder):
-        decryption_data = dec_only_base64(category_item, master_password)
+        decryption_data = dec_only_base64(category_item, generic_key)
         s += 1
         print(BLUE, str(s) + '.', YELLOW, decryption_data, DEFAULT_COLOR)
     if category == 'resource':
         backup_message = ''
         if os.path.exists(OLD_ELBA):
-            backup_message = '\n  - Enter "-o" to rollback'
-        print(BLUE +
-              '\n  - Enter "-r" to restart, "-x" to exit'
-              '\n  - Enter "-a" to add new resource'
-              '\n  - Enter "-c" to change master-password'
-              '\n  - Enter "-d" to remove resource'
-              '\n  - Enter "-n" to go to notes'
-              '\n  - Enter "-u" to update program'
-              '\n  - Enter "-z" to remove ALL data', backup_message,
-              YELLOW,
-              '\n Select resource by number \n', DEFAULT_COLOR)
+            backup_message = BLUE + '\n  - Enter "-o" to rollback               ' + YELLOW + ' |'
+        print(
+            YELLOW, '\n__________________________________________',
+            BLUE, '\n  - Enter "-r" to restart, "-x" to exit ', YELLOW, '|',
+            BLUE, '\n  - Enter "-a" to add new resource      ', YELLOW, '|',
+            BLUE, '\n  - Enter "-c" to change master-password', YELLOW, '|',
+            BLUE, '\n  - Enter "-d" to remove resource       ', YELLOW, '|',
+            BLUE, '\n  - Enter "-n" to go to notes           ', YELLOW, '|',
+            BLUE, '\n  - Enter "-u" to update program        ', YELLOW, '|',
+            BLUE, '\n  - Enter "-z" to remove ALL data       ', YELLOW, '|',
+            backup_message, YELLOW,
+            '\n__________________________________________|\n'
+            '\n Select resource by number \n', DEFAULT_COLOR)
     if category == 'note':
-        print(BLUE + '\n  - Press "Enter" to go back'
-                     '\n  - Enter "-a" to add new note'
-                     '\n  - Enter "-x" to exit'
-                     '\n  - Enter "-d" to remove note',
-              YELLOW, '\n Select note by number \n', DEFAULT_COLOR)
+        print(
+            YELLOW, '\n__________________________________',
+            BLUE, '\n  - Press "Enter" to go back    ', YELLOW, '|',
+            BLUE, '\n  - Enter "-a" to add new note  ', YELLOW, '|',
+            BLUE, '\n  - Enter "-x" to exit          ', YELLOW, '|',
+            BLUE, '\n  - Enter "-d" to remove note   ', YELLOW, '|',
+            '\n__________________________________|\n',
+            YELLOW, '\n Select note by number \n', DEFAULT_COLOR)
     if s == 0:
         print(YELLOW + '\n    No' + ' saved ' + category + 's \n')
