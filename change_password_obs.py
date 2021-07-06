b8207dbe1400585590e9b30f557d0a363e59a49d
Rostislav <fadenougat@gmail.com>
2021-07-06 10:53:09 +0300

    Убраны лишние импорты

--- change_password_obs.py
+++ change_password_obs.py
@@ -1,20 +1,16 @@
 from main import *
-from enc_obs import save_data_to_file
 
-from csv import DictReader, DictWriter
-from enc_obs import enc_aes, dec_aes, enc_only_base64, dec_only_base64
-from datetime_obs import greeting
+from enc_obs import enc_aes, dec_aes
 from actions_with_password_obs import create_and_confirm_user_password
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from stdiomask import getpass
 
 from time import sleep
-from shutil import copyfile
 import os
 
 
-__version__ = '3.0.0'
+__version__ = '3.0.1'
 
 
 def change_master_password():
