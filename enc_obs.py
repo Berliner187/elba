d6f7643d54421e5150bfc7eec407ea11b95d6aa3
Rostislav <fadenougat@gmail.com>
2021-07-19 18:39:39 +0300

    Исправлен текст шифрования файлов

--- enc_obs.py
+++ enc_obs.py
@@ -34,7 +34,7 @@ except ModuleNotFoundError as error_module:
     quit()
 
 
-__version__ = '6.1.3'
+__version__ = '6.1.4'
 
 
 class AESCipher(object):
@@ -182,7 +182,7 @@ class WorkWithUserFiles:
             progress_status = ((now * 100) // total)
             to_print = ''
             if type_work == 'files':
-                to_print = ' The process is completed on'
+                to_print = ' File encryption completed on'
             elif type_work == 'folders':
                 to_print = ' Folder creation status'
             print(YELLOW, to_print, DEFAULT_COLOR, progress_status, '%')
