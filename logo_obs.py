dc12c3505236295d350cccdd12bd9ff8c3bfa260
Berliner <57003925+Berliner187@users.noreply.github.com>
2022-05-19 21:16:52 +0300

    Полностью переписан старт программы, улучшения в коде

--- logo_obs.py
+++ logo_obs.py
@@ -11,24 +11,28 @@ import os
 from main import *
 
 
-__version__ = '0.9-00'
+__version__ = '0.10-00'
 
 
 cols = get_size_of_terminal()
 
 
-def wait_effect(lines, sleeper):
+def iterate_over_characters(string, sleeper):
     crutch = sleeper
+    for c in string.center(cols):
+        if c == ' ':
+            sleeper = 0
+        else:
+            sleeper = crutch
+        print(c, end='')
+        sys.stdout.flush()
+        sleep(sleeper)
+    print('')
+
+
+def wait_effect(lines, sleeper):
     for line in lines:
-        for c in line.center(cols):
-            if c == ' ':
-                sleeper = 0
-            else:
-                sleeper = crutch
-            print(c, end='')
-            sys.stdout.flush()
-            sleep(sleeper)
-        print('')
+        iterate_over_characters(line, sleeper)
 
 
 logo_strings_row = [
@@ -43,6 +47,20 @@ logo_strings_row = [
         " E            LL            B         BB   AA        AA     ",
         " EEEEEEEEEEE  LLLLLLLLLLL   BBBBBBBBBBB    AA        AA     "
     ]
+
+
+hello = [
+        "",
+        " HH        HH   EEEEEEEE      LL            LL             OOOOOOOOO           ",
+        " HH        HH   EE            LL            LL            OO       OO          ",
+        " HH        HH   EE            LL            LL            OO       OO          ",
+        " HHHHHHHHHHHH   EEEEEEE       LL            LL            OO       OO          ",
+        " HH        HH   EE            LL            LL            OO       OO          ",
+        " HH        HH   EE            LL            LL            OO       OO          ",
+        " HH        HH   EE            LL            LL            OO       OO          ",
+        " HH        HH   EE            LL            LL            OO       OO          ",
+        " HH        HH   EEEEEEEEEEE   LLLLLLLLLLL   LLLLLLLLLLL    OOOOOOOOO           "
+]
 
 
 author_emb = [
@@ -60,7 +78,7 @@ author_emb = [
 
 def logo():
     # <<< Посимвольная отрисовка лого >>>
-    wait_effect(logo_strings_row, 0.0000003)
+    wait_effect(logo_strings_row, 0)
 
 
 def author():
@@ -90,11 +108,6 @@ def animation():
                 print(ACCENT_1)
             wait_effect(logo_strings_row, 0.00001)
             sleep(.1)
-
-
-def elba():
-    print(ACCENT_1)
-    logo()
 
 
 def Ukraine():
@@ -128,14 +141,36 @@ def Ukraine():
     animation()
 
 
+def elba():
+    print(ACCENT_5)
+    logo()
+
+
 def first_start_message():
-    lines = [
-        ACCENT_3,
-        "  - Encrypt your passwords with one master-password -  ",
-        "  -           No resources saved. Add them!         -  ",
-        " ----                That's easy!                 ---- ",
-        RED,
-        "          Программа не поддерживает русский язык       ",
-        ACCENT_4
+    """ Сообщения в самом начале """
+    elba_say_list = [
+        'Your data is kept confidential through Elba.',
+        'Only you know your service passwords',
+        'Your notes will remain secret',
+        RED + 'Нет поддержки русского языка'
     ]
-    wait_effect(lines, 0.03)
+
+    def template_elba_say():
+        for string in elba_say_list:
+            sleep(1)
+            iterate_over_characters(standard_location('/SAY') + string, 0.03)
+            sleep(1)
+
+    sleep(2)
+    system_action('clear')
+    print(ACCENT_4)
+    wait_effect(hello, 0.05)    # Приветствие
+    sleep(3)
+    system_action('clear')
+    print(ACCENT_5)
+    wait_effect(logo_strings_row, 0.03)
+    print(ACCENT_3)
+    template_elba_say()
+    input(ACCENT_4 + '\n Continue...')
+    system_action('clear')
+# first_start_message()
