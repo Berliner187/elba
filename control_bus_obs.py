014d6c1f97d9d6ae8d22dcfb98d886736881f5b6
Berliner <57003925+Berliner187@users.noreply.github.com>
2022-05-24 14:42:26 +0300

    0.10.02: Рефакторинг и улучшения в коде

--- control_bus_obs.py
+++ control_bus_obs.py
@@ -19,11 +19,12 @@ import settings_obs
 import info_obs
 import rollback_obs
 import security_obs
+import passwords_obs
 
 from main import *
 
 
-__version__ = '0.10-01'
+__version__ = '0.10-02'
 
 
 def control_bus(generic_key):
@@ -32,41 +33,45 @@ def control_bus(generic_key):
     def drawing_instructions():
         functions_obs.ProgramFunctions(generic_key, 'resource').get_category_label()
 
-    change_resource_or_actions = input(standard_location('')).lower()   # Выбор действия
+    user_actions = input(standard_location('')).lower()   # Выбор действия
     try:
-        if '-a' in change_resource_or_actions:  # Добавление нового ресурса
+        if '-a' in user_actions:  # Добавление нового ресурса
             system_action('clear')
+            write_log("Add resource", "RUN")
             resources_obs.add_new_resource(generic_key)
             drawing_instructions()
             write_log("Add resource", "QUIT")
 
-        if '-x' in change_resource_or_actions:  # Выход
+        if '-x' in user_actions:  # Выход
             system_action('clear')
             template_some_message(ACCENT_3, '--- ELBA CLOSED ---')
-            write_log("ELBA", "CLOSE")
+            write_log("ELBA", "QUIT")
             quit()
 
-        elif '-r' in change_resource_or_actions:  # Перезапуск
+        elif '-r' in user_actions:  # Перезапуск
             system_action('clear')
             write_log("Restart", "OK")
             system_action('restart')
 
-        elif '-c' in change_resource_or_actions:    # Смена мастер-пароля
+        elif '-c' in user_actions:    # Смена мастер-пароля
             system_action('clear')
+            write_log("Change password", "RUN")
             change_mp_obs.change_master_password()
             write_log("Change password", "QUIT")
 
-        elif '-d' in change_resource_or_actions:    # Удаление ресурса
+        elif '-d' in user_actions:    # Удаление ресурса
+            write_log("Remove object", "RUN")
             remove_obs.remove_object('resource')
-            functions_obs.ProgramFunctions(generic_key, 'resource').get_category_label()
+            drawing_instructions()
             write_log("Remove object", "QUIT")
 
-        elif '-n' in change_resource_or_actions:    # Добавление заметок
+        elif '-n' in user_actions:    # Добавление заметок
+            write_log("Notes", "RUN")
             functions_obs.ProgramFunctions(generic_key, 'note').get_category_label()
             notes_obs.notes(generic_key)
-            write_log("Exit from notes", "QUIT")
+            write_log("Notes", "QUIT")
 
-        elif '-f' in change_resource_or_actions:    # Шифрование файлов
+        elif '-f' in user_actions:    # Шифрование файлов
             functions_obs.ProgramFunctions(generic_key, 'encryption').get_category_label()
             change_action = input(ACCENT_4 + standard_location('/ENCRYPTION') + ACCENT_4)
             if '-e' in change_action:
@@ -81,29 +86,33 @@ def control_bus(generic_key):
             write_log("Exit from encrypt", "QUIT")
             functions_obs.ProgramFunctions(generic_key, 'resource').get_category_label()
 
-        elif '-u' in change_resource_or_actions:    # Обновление программы
+        elif '-u' in user_actions:    # Обновление программы
             system_action('clear')
+            write_log("Update", "RUN")
             update_obs.update()
             functions_obs.ProgramFunctions(generic_key, 'resource').get_category_label()
             write_log("Update", "OK")
 
-        elif '-z' in change_resource_or_actions:    # Удаление всех данных
+        elif '-z' in user_actions:    # Удаление всех данных
             system_action('clear')
+            template_some_message(ACCENT_3, 'Verify master password')
+            passwords_obs.ActionsWithPassword(None).verify_master_password(False)
             template_some_message(RED, ' - Are you sure you want to delete all data? - ')
-            change_yes_or_no = input(ACCENT_1 + ' - Remove ALL data? (y/n): ' + ACCENT_4)
-            if change_yes_or_no == 'y':
+            confirm = template_question('Remove ALL data?')
+            if confirm == 'y':
                 template_remove_folder(FOLDER_WITH_DATA)
                 system_action('clear')
                 quit()
             drawing_instructions()
 
-        elif '-i' in change_resource_or_actions:
+        elif '-i' in user_actions:
             system_action("clear")
+            write_log("Info", "RUN")
             info_obs.Information().get_info()
             write_log("Get info", "QUIT")
             control_bus(generic_key)
 
-        elif '-l' in change_resource_or_actions:
+        elif '-l' in user_actions:    # Чтение файла с логами
             system_action("clear")
             functions_obs.StylishLook().topper('LOGS')
             log_data = open(FILE_LOG, 'r')
@@ -111,51 +120,53 @@ def control_bus(generic_key):
             try:
                 for line in reader_log:
                     print(
-                        line[FIELDS_LOG_FILE[0]],
-                        line[FIELDS_LOG_FILE[1]],
-                        line[FIELDS_LOG_FILE[2]],
-                        line[FIELDS_LOG_FILE[3]]
+                        "{:s} -- {:19s} --- {:5s} -- {:50s}".format(
+                            line[FIELDS_LOG_FILE[0]], line[FIELDS_LOG_FILE[1]],
+                            line[FIELDS_LOG_FILE[3]], line[FIELDS_LOG_FILE[2]]
+                        )
                     )
-                write_log("Check logs", "QUIT")
+                write_log("Check logs", "OK")
             except KeyError as error:
-                write_log(error, "FAILED")
+                write_log(error, "FAIL")
             template_some_message(ACCENT_1, " - Press Enter to exit - ")
 
-        elif '-o' in change_resource_or_actions:    # Откат к старой сохраненной версии
+        elif '-o' in user_actions:    # Откат к старой сохраненной версии
             if os.path.exists(OLD_ELBA) is False:
                 template_some_message(ACCENT_1, '- No versions saved - ')
             else:
                 write_log("Try roll back", "RUN")
                 rollback_obs.rollback()
-                write_log("Success roll back", "QUIT")
+                write_log("Roll back", "OK")
                 system_action('restart')
 
-        elif '-s' in change_resource_or_actions:    # Пользовательские настройки
+        elif '-s' in user_actions:    # Пользовательские настройки
             write_log('Settings', 'Run')
             settings_obs.settings()
             functions_obs.ProgramFunctions(generic_key, 'resource').get_category_label()
-            write_log('Settings', 'Exit')
+            write_log('Settings', 'QUIT')
 
-        elif change_resource_or_actions.isnumeric():   # Вывод сохраненных данных о ресурсе
+        elif user_actions.isnumeric():   # Вывод сохраненных данных о ресурсе
             match_string = 0
             for resource_in_folder in os.listdir(FOLDER_WITH_RESOURCES):
                 match_string += 1
-                if match_string == int(change_resource_or_actions):
+                if match_string == int(user_actions):
                     system_action('clear')
                     drawing_instructions()
 
                     def template_print_decryption_data(data_type, value):
                         print(
-                            ACCENT_3, data_type, ACCENT_4, security_obs.dec_aes(value, generic_key)
+                            " {:8s} {:s}---{:s} {:s}".format(
+                                data_type, ACCENT_2, ACCENT_4,
+                                security_obs.dec_aes(value, generic_key))
                         )
 
                     path_to_resource = FOLDER_WITH_RESOURCES + resource_in_folder
                     template_print_decryption_data(
-                        'Resource --->', f"{path_to_resource}/{FILE_RESOURCE}")
+                        'Resource', f"{path_to_resource}/{FILE_RESOURCE}")
                     template_print_decryption_data(
-                        'Login ------>', f"{path_to_resource}/{FILE_LOGIN}")
+                        'Login', f"{path_to_resource}/{FILE_LOGIN}")
                     template_print_decryption_data(
-                        'Password --->', f"{path_to_resource}/{FILE_PASSWORD}")
+                        'Password', f"{path_to_resource}/{FILE_PASSWORD}")
             if match_string == 0:
                 drawing_instructions()
         else:
