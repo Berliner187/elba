75217c8e20d9292a5fa818f2115fbb51ddb55cd1
Berliner <57003925+Berliner187@users.noreply.github.com>
2022-06-06 12:31:11 +0300

    Какие-то обновления в паролях

--- passwords_obs.py
+++ passwords_obs.py
@@ -9,6 +9,7 @@ from main import *
 import security_obs
 import logo_obs
 import functions_obs
+import getpass_obs
 from getpass_obs import getpass
 
 from time import sleep
@@ -19,47 +20,10 @@ import random
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-__version__ = '0.10-03'
+__version__ = '0.10-04'
 
 
 cols = get_size_of_terminal()
-
-
-def create_and_confirm_user_password():
-    """ Создание и подтверждение пользовательского пароля """
-    print(ACCENT_3, '— Minimum password length — 8 characters')
-    print(ACCENT_3, '— Your password must contain lowercase letters')
-    print(ACCENT_3, '— Your password must contain upper letters')
-    print(ACCENT_3, '— Your password must contain numbers')
-    print('\n')
-
-    def template_red_messages(message):
-        system_action('clear')
-        print(f"{RED}\n\n - {message}{ACCENT_4} -")
-
-    while True:
-        password = getpass(f"{ACCENT_1} Password: {ACCENT_4}")
-        confirm_password = getpass(f"{ACCENT_1} Confirm:  {ACCENT_4}")
-        if confirm_password == 'x':
-            quit()
-        if len(password) < 8:
-            template_red_messages("Make sure your password is at lest 8 letters".center(cols))
-        elif len(password) > 64:
-            template_red_messages("Maximální délka hesla - 64 Symbol".center(cols))
-        elif password != confirm_password:
-            template_red_messages("Passwords don't match".center(cols))
-        elif re.search('[0-9]', password) is None:
-            template_red_messages("Make sure your password has a number in it".center(cols))
-        elif re.search('[a-z]', password) is None:
-            template_red_messages("Make sure your password has lower case letters".center(cols))
-        # elif re.search('[A-Z]', password) is None:
-        #     template_red_messages("Make sure your password has upper case letters".center(cols))
-        elif password == 'x':
-            quit()
-        elif password == confirm_password:
-            return password
-        else:
-            template_red_messages("-_-".center(cols))
 
 
 class ActionsWithPassword:
@@ -70,23 +34,55 @@ class ActionsWithPassword:
     def get_password(self):
         """ Получение паролей """
 
-        def generation_new_password(length_password, add_random_symbols):
-            """ Функция создания случайного пароля """
-            symbols_for_password = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-            additional_symbols = '!#$*()_-+[]{}?'
-            if add_random_symbols:
-                symbols_for_password += additional_symbols
+        def create_and_confirm_user_password():
+            """ Создание и подтверждение пользовательского пароля """
+            print(ACCENT_3, '— Minimum password length — 8 characters')
+            print(ACCENT_3, '— Your password must contain lowercase letters')
+            print(ACCENT_3, '— Your password must contain upper letters')
+            print(ACCENT_3, '— Your password must contain numbers')
+            print('\n')
+
             while True:
-                if length_password >= 8:
+                new_password = getpass(f"{ACCENT_1} Password: {ACCENT_4}")
+                confirm_new_password = getpass(f"{ACCENT_1} Confirm:  {ACCENT_4}")
+                if confirm_new_password == 'x':
+                    quit()
+                if len(new_password) < 8:
+                    template_some_message(RED, "Make sure your password is at lest 8 letters")
+                elif len(new_password) > 256:
+                    template_some_message(RED, "Maximální délka hesla - 256 Symbol")
+                elif new_password != confirm_new_password:
+                    template_some_message(RED, "Passwords don't match")
+                elif re.search('[0-9]', new_password) is None:
+                    template_some_message(RED, "Make sure your password has a number in it")
+                elif re.search('[a-z]', new_password) is None:
+                    template_some_message(RED, "Make sure your password has lower case letters")
+                # elif re.search('[A-Z]', new_password) is None:
+                #     template_some_message(RED, "Make sure your password has upper case letters")
+                else:
+                    return new_password
+
+        def generation_new_password(length_password, add_random_symbols):
+            """ Генерирование нового пароля """
+            symbols_for_password = list(range(65, 91)) + list(range(97, 123)) + list(range(0, 10))
+            if add_random_symbols:
+                symbols_for_password += [33, 35, 36, 40, 41, 42, 43, 45, 60, 61, 62, 63, 95, 123, 125]
+            while True:
+                if 8 <= length_password < 256:
                     new_password = ''
                     for i in range(length_password):
-                        new_password += random.choice(symbols_for_password)
+                        random_choice = random.choice(symbols_for_password)
+                        if len(str(random_choice)) == 1:
+                            new_password += str(random_choice)
+                        else:
+                            new_password += chr(random_choice)
                     return new_password
                 elif length_password < 8:
-                    template_some_message(RED, ' The length must be at least 8 characters \n')
-                    length_password = int(input(ACCENT_1 + ' - Length: ' + ACCENT_4))
-                elif len(password) > 64:
-                    template_red_messages("Maximální délka hesla - 64 Symbol".center(cols))
+                    template_some_message(RED, "Die Länge muss mindestens 8 Zeichen betragen")
+                    length_password = int(template_input("Length: "))
+                elif length_password > 256:
+                    template_some_message(RED, "Maximale Passwortlänge - 256 Symbol")
+                    length_password = int(template_input("Length: "))
 
         # Создание мастер-пароля, создание хеша и сохранение в файл
         if self.type_pas == 'master':
@@ -106,15 +102,15 @@ class ActionsWithPassword:
                         return master_password
 
         # Сохранение пользовательского пароля для ресурсов
-        elif self.type_pas == 'self':
-            template_some_message(ACCENT_1, "- Pick a self password - \n")
+        elif self.type_pas == 'own_password':
+            template_some_message(ACCENT_1, "- Pick an own password - \n")
             password = create_and_confirm_user_password()
             template_some_message(GREEN, "--- Your password success saved! ---")
             sleep(1)
             return password
 
         # Получение нового сгенерированного пароля
-        elif self.type_pas == 'gen_new':
+        elif self.type_pas == 'generating':
             length_new_pas = int(input(f"{ACCENT_1}- Length: {ACCENT_4}"))
             status_adding_characters = False
             request_for_adding_characters = input(
@@ -122,18 +118,16 @@ class ActionsWithPassword:
             )
             if request_for_adding_characters == 'y':
                 status_adding_characters = True
-            password = generation_new_password(length_new_pas, status_adding_characters)
-            print(
-                f'{ACCENT_3}\n - Your new password - '
-                f'{GREEN}{password}{ACCENT_3}'
-                f' - success saved{ACCENT_4}'
+            password = generation_new_password(length_new_pas, add_random_symbols=status_adding_characters)
+            template_some_message(
+                ACCENT_3, f'- Your new password - {GREEN}{password}{ACCENT_3} - success saved'
             )
             sleep(3)
             return password
 
         # Получение общего ключа
         elif self.type_pas == 'generic':
-            generic = generation_new_password(random.randrange(2**5, 2**6), False)
+            generic = generation_new_password(random.randrange(2**5, 2**6), add_random_symbols=False)
             hash_to_file = generate_password_hash(generic)
             hash_gen = open(FILE_WITH_HASH_GENERIC_KEY, 'w')
             hash_gen.write(hash_to_file)
@@ -142,10 +136,10 @@ class ActionsWithPassword:
 
     @staticmethod
     def verify_master_password(show_logotype):
-        """ Проверка мастер-пароля """
+        """ Подтверждение мастер-пароля """
         def get_master_password():
             input_master_password = getpass(
-                f"{ACCENT_1}\n\n   {standard_location('/LOGIN')}{ACCENT_4}"
+                f"{ACCENT_1}{standard_location('/LOGIN')}{ACCENT_4}"
             )
             if input_master_password == 'x':
                 quit()
@@ -155,13 +149,13 @@ class ActionsWithPassword:
                 logo_obs.animation()
             return input_master_password
 
-        cnt_left = 3
+        cnt_left = 2
         while True:
             if show_logotype:   # Вывод версии и логотипа
                 logo_obs.show_name_program()
                 logo_obs.elba()
             master_password = get_master_password()
-            # Проверка хэша пароля
+            # Проверка хеша пароля
             hash_password = check_password_hash(open(FILE_WITH_HASH).readline(), master_password)
             if (CHECK_FILE_WITH_HASH is False) or (CHECK_FILE_WITH_GENERIC is False):
                 if CHECK_FOLDER_FOR_RESOURCE:
@@ -170,17 +164,32 @@ class ActionsWithPassword:
             # Если нет подтверждения
             if hash_password is False:
                 system_action('clear')
-                print(RED, '\n'*3, "-----  Wrong password  -----".center(cols), '\n'*3, ACCENT_3)
-                print(' '*16, f"Attempts left: {RED}{cnt_left}{ACCENT_4}".center(cols))
+                template_warning_message(RED, '--- PASSWORD IS WRONG ---')
+                print('\n'*3)
+                color_cnt_left = ACCENT_5
+                if cnt_left == 0:
+                    color_cnt_left = RED
+                print(' ' * 16, f"{color_cnt_left}{cnt_left}{ACCENT_4} TRIES LEFT".center(cols))
                 cnt_left -= 1
                 sleep(1)
                 system_action('clear')
-                if cnt_left <= 0:
+                if cnt_left < 0:
                     system_action('clear')
-                    template_some_message(RED, "---  Limit is exceeded  ---")
+                    template_warning_message(RED, "--- LIMIT IS EXCEEDED ---")
                     write_log('Someone tried to enter', 'ALERT')
                     sleep(1)
-                    animation()
+                    while True:
+                        user_try_unlock = getpass_obs.getpass('')
+                        # lock_hash = generate_password_hash('')
+                        if check_password_hash(
+                                'pbkdf2:sha256:260000$'
+                                '1zRcjkY2Ve5upTEI$3c3a'
+                                'be130d7a70a4972c33116'
+                                'b3c647b4cfb65b8729cc8'
+                                '12fbbfcc61e86a837e',
+                                user_try_unlock
+                        ):
+                            quit()
             # Если есть подтверждение
             else:
                 if hash_password and CHECK_FOLDER_FOR_RESOURCE:
@@ -199,29 +208,33 @@ class ActionsWithPassword:
                         quit()
 
 
-def choice_generation_or_save_self_password(resource, login, master_password):
-    """ Выбор пароля: генерирование нового или сохранение пользовательского """
-    proposed_options = [
+def creating_new_password(resource, login, master_password):
+    """ Выбор пароля для ресурса (сервиса) и сохранение в файл """
+
+    def get_from_user_new_generation_password():
+        password = ActionsWithPassword('generating').get_password()
+        security_obs.save_data_to_file(resource, login, password, master_password, 'resource')
+
+    def get_from_user_password():
+        password = ActionsWithPassword('own_password').get_password()
+        security_obs.save_data_to_file(resource, login, password, master_password, 'resource')
+
+    lines_proposed_options = [
         "Generation new password",
         "Save your password"
     ]
 
+    actions_with_offered_options = {
+        1: get_from_user_new_generation_password,
+        2: get_from_user_password
+    }
+
     print('\n')
-    functions_obs.StylishLook().scrolling_and_numbering_content(proposed_options)
+    functions_obs.StylishLook().scrolling_and_numbering_content(lines_proposed_options)
     print('\n')
 
-    while True:
-        change_type = int(input('Change (1/2): '))
-        if change_type == 1:  # Генерирование пароля и сохранение в файл
-            password = ActionsWithPassword('gen_new').get_password()
-            security_obs.save_data_to_file(resource, login, password, master_password, 'resource')
-            # Костыль для выхода из цикла
-            return 0
-        elif change_type == 2:  # Сохранение пользовательского пароля
-            password = ActionsWithPassword('self').get_password()
-            security_obs.save_data_to_file(resource, login, password, master_password, 'resource')
-            return 0
-        else:   # Если ошибка выбора
-            print(f"{RED}\n  -- Error of change. Please, change again -- {ACCENT_4}")
-            sleep(1)
-        system_action('clear')
+    user_change = int(template_input('Change (1-2): '))
+    try:
+        actions_with_offered_options[user_change]()
+    except KeyError:
+        pass
--- resources_obs.py
+++ resources_obs.py
@@ -1,19 +1,19 @@
 # -*- coding: UTF-8 -*-
 from main import *
 
-import passwords_obs
-import functions_obs
+from passwords_obs import creating_new_password
+from functions_obs import ProgramFunctions
 
 
-__version__ = '0.10-01'
+__version__ = '0.10-02'
 
 
 def add_new_resource(generic_key):
-    """ Добавление нового ресурса """
-    template_some_message(ACCENT_3, '||| ADD NEW RESOURCE |||')
-    resource = input(ACCENT_1 + ' Resource: ' + ACCENT_4)
+    """ Добавление нового ресурса (сервиса) """
+    template_some_message(ACCENT_3, '--- ADD NEW SERVICE ---')
+    service = input(ACCENT_1 + ' Service: ' + ACCENT_4)
     login = input(ACCENT_1 + ' Login: ' + ACCENT_4)
     # Передача аргументов для выбора пароля
-    passwords_obs.choice_generation_or_save_self_password(resource, login, generic_key)
+    creating_new_password(service, login, generic_key)
     # Возвращение в лейбл по умолчанию
-    functions_obs.ProgramFunctions(generic_key, 'resource').get_category_label()
+    ProgramFunctions(generic_key, 'resource').get_category_label()
