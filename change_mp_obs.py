c33c6e539fcd83712469951ab44113f8c59ecbcd
Berliner <57003925+Berliner187@users.noreply.github.com>
2022-06-13 00:13:23 +0300

    Изменены сообщения

--- change_mp_obs.py
+++ change_mp_obs.py
@@ -1,10 +1,10 @@
 from main import *
 
-from security_obs import *
+from security_obs import enc_aes, dec_aes
 from getpass_obs import getpass
 
 import passwords_obs
-import functions_obs
+from functions_obs import StylishLook
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -21,23 +21,23 @@ def change_master_password():
     def get_confirm_master_password():
         while True:
             system_action('clear')
-            functions_obs.StylishLook().topper('CHANGE_MASTER_PASSWORD')
+            StylishLook().topper('CHANGE_MASTER_PASSWORD')
             _confirm_master_password = getpass(ACCENT_1 + standard_location('/LOGIN') + ACCENT_4)
             open_file_with_hash = open(FILE_WITH_HASH).readline()
             check_master_password = check_password_hash(open_file_with_hash, _confirm_master_password)
             if check_master_password:
                 return _confirm_master_password
             else:
-                template_some_message(RED, '--- Wrong master-password ---')
+                template_warning_message(RED, '--- PASSWORD IS WRONG ---')
                 sleep(1)
 
-    functions_obs.StylishLook().topper('CHANGE_MASTER_PASSWORD')
+    StylishLook().topper('CHANGE_MASTER_PASSWORD')
     confirm_master_password = get_confirm_master_password()
     system_action('clear')
-    template_some_message(GREEN, '--  Success confirm  --')
+    template_some_message(GREEN, '---  Success confirm  ---')
     sleep(.6)
     system_action('clear')
-    template_some_message(ACCENT_3, ' - Pick a new master-password -')
+    template_some_message(ACCENT_3, '- Pick a new master-password -')
     new_master_password = passwords_obs.create_and_confirm_user_password()
     # Generic-key шифруется новым мастер-паролем
     generic_key_from_file = dec_aes(FILE_WITH_GENERIC_KEY, confirm_master_password)
