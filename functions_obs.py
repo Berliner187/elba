e75126eb0d49d0f355572091f0da91fc6db3aa83
Berliner <57003925+Berliner187@users.noreply.github.com>
2022-06-13 00:11:41 +0300

    Упрощены названия функций

--- functions_obs.py
+++ functions_obs.py
@@ -8,6 +8,7 @@
 from main import *
 import random
 import security_obs
+import shutil
 
 
 __version__ = '0.10-03'
@@ -36,9 +37,9 @@ class StylishLook:
         hollow_filled_string = filling(cols - 1)
         middle_string = filling(((cols - 1) - (len(location_above) + 4)))
 
-        topper(hollow_filled_string)  # 1 строка
+        topper(hollow_filled_string)                                         # 1 строка
         topper(f"19 {ACCENT_5}{location_above}{ACCENT_2} " + middle_string)  # 2 строка
-        topper(hollow_filled_string)  # 3 строка
+        topper(hollow_filled_string)                                         # 3 строка
         print(ACCENT_4, "\n" * 2)
 
     @staticmethod
@@ -46,15 +47,16 @@ class StylishLook:
         cnt = 0
         for content in what_scrolls:
             cnt += 1
-            print(f" {ACCENT_3}[{ACCENT_1}{cnt}{ACCENT_3}] - {ACCENT_1}{content}{ACCENT_4}")
+            print(f" {ACCENT_3}[{ACCENT_1}{cnt}{ACCENT_3}] - {content}{ACCENT_4}")
         return cnt
 
 
 class ProgramFunctions(object):
     """
-        1. Показ сохраненных ресурсов и действий в этой категории
-        2. Показ сохраненных заметок и действий в этой категории
-        3. Показ наименований папок, в которых были зашифрованы файлы,
+        Вывод инструкций к выбранной категории
+        1. Отображение сохраненных сервисов и действий в этой категории
+        2. Отображение сохраненных заметок и действий в этой категории
+        3. Отображение наименований директорий, в которых были зашифрованы файлы,
         а также действий в этой категории
     """
 
@@ -76,6 +78,7 @@ class ProgramFunctions(object):
 
         StylishLook().topper(self.category + 'S')
 
+        # Прокрутка сохраненных данных
         number_saved_data = 0
         for category_item in os.listdir(type_folder):
             if self.category != 'encryption':
@@ -87,7 +90,7 @@ class ProgramFunctions(object):
         if number_saved_data == 0:
             print(f"{ACCENT_1}   No saved {self.category}s {ACCENT_4}")
 
-        # <<< Показ инструкций, которые возможны для выполнения в данном окне >>>
+        # <<< Отображение инструкций, которые возможны для выполнения в данном окне >>>
         def template_show_functions(key, message):
             """ Шаблон инструкций для пользователя """
             return f"{ACCENT_3} [{ACCENT_1}{key}{ACCENT_3}]  —  {message}"
@@ -100,37 +103,37 @@ class ProgramFunctions(object):
                 backup_message = template_show_functions('-O', 'Rollback')
             lines_instruction = [
                 ACCENT_3,
-                template_show_functions('-A', 'Add new resource'),
-                template_show_functions('-D', 'Remove resource'),
-                template_show_functions('-N', 'Go to notes'),
-                template_show_functions('-F', 'Encrypt your files'),
-                template_show_functions('-C', 'Change master-password'),
-                template_show_functions('-S', 'Go to settings'),
-                template_show_functions('-U', 'Update program'),
+                template_show_functions('-A', 'Add New Resource'),
+                template_show_functions('-D', 'Remove Resource'),
+                template_show_functions('-C', 'Change Master-Password'),
+                template_show_functions('-F', 'File Encryption'),
+                template_show_functions('-N', 'Notes'),
+                template_show_functions('-S', 'Settings'),
+                template_show_functions('-U', 'Update'),
                 backup_message,
                 template_show_functions('-R', 'Restart'),
                 template_show_functions('-X', 'Exit'),
-                template_show_functions('-Z', 'Remove ALL data')
+                template_show_functions('-Z', 'Remove ALL Data')
             ]
 
         # Для заметок:
         elif self.category == 'note':
             lines_instruction = [
                 ACCENT_3,
-                template_show_functions("-A", 'Add new note'),
-                template_show_functions("-D", 'Remove note'),
+                template_show_functions("-A", 'Add New Note'),
+                template_show_functions("-D", 'Remove Note'),
                 template_show_functions("Enter", 'Go back')
             ]
 
-        # Для шифрованных файлов:
+        # Для файлов, которые были зашифрованы/дешифрованы:
         elif self.category == 'encryption':
             template_some_message(ACCENT_3,
-                                  f"-- Go to the {FOLDER_WITH_ENC_DATA} "
-                                  f"data folder and follow the instructions below --")
+                                  "-- Go to the " + ACCENT_1 + FOLDER_WITH_ENC_DATA + ACCENT_3 +
+                                  " data folder and follow the instructions below --")
             lines_instruction = [
                 ACCENT_3,
-                template_show_functions('-E', 'Encryption files'),
-                template_show_functions('-D', 'Decryption files'),
+                template_show_functions('-E', 'Encrypt File'),
+                template_show_functions('-D', 'Decrypt File'),
                 template_show_functions('Enter', 'Go back')
             ]
 
