4a6a7ea781a296bc353b964b1c047eba40af316d
Berliner <57003925+Berliner187@users.noreply.github.com>
2022-06-13 00:12:58 +0300

    Небольшие изменения

--- datetime_obs.py
+++ datetime_obs.py
@@ -15,29 +15,27 @@ import datetime
 from time import sleep
 
 
-__version__ = '0.10-00'
+__version__ = '0.10-02'
 
-SLEEPER_WAIT = .015     # Константа задержки вывода
+SLEEPER_WAIT = .022     # Константа задержки вывода
 
 
 def greeting(generic_key):   # Greeting Depending On Date Time
-    """ Функция вывода приветствия юзера """
+    """ Функция отображения приветствия """
     def template_greeting(times_of_day):
         if os.path.exists(FILE_USER_NAME) is False:  # Создание файла с именем
             system_action('clear')
-            message_about_enter_nickname = '\n Enter your nickname'
             print(ACCENT_3)
-            logo_obs.iterate_over_characters(message_about_enter_nickname, SLEEPER_WAIT)
-            name = template_input('Nickname:')
+            logo_obs.iterate_over_characters(' Enter your nickname', SLEEPER_WAIT)
+            name = template_input(' Nickname:')
             security_obs.enc_aes(FILE_USER_NAME, name, generic_key)
         else:  # Чтение из файла с именем и вывод в консоль
             name = security_obs.dec_aes(FILE_USER_NAME, generic_key)
         lines = [
-            f'{ACCENT_1} ',
-            f'{times_of_day} {name}',
-            f'{ACCENT_4}',
+            f'{times_of_day} {name}'
         ]
         logo_obs.wait_effect(lines, SLEEPER_WAIT)
+        sleep(SLEEPER_WAIT)
     hms = datetime.datetime.today()
     time_now = hms.hour * 3600 + hms.minute * 60 + hms.second  # Время в секундах
     if 14400 <= time_now < 43200:
@@ -48,6 +46,4 @@ def greeting(generic_key):   # Greeting Depending On Date Time
         template_greeting('Good evening,')
     elif 0 <= time_now < 14400:
         template_greeting('Good night,')
-    else:
-        write_log("Time: Oops..", "FAIL")
     sleep(.3)
